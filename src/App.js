17bc65bc9ad78a71ba2e3b0e2e64e94aa6458ba2
Growthack90 <hackmaker90@outlook.it>
2021-04-18 18:17:49 +0200

    state ciclo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
-import margherita from "./images/margherita.jpg"
+import margherita from "./images/margherita.jpg";
+import bianca from "./images/bianca.jpg";
+import capricciosa from "./images/capricciosa.jpg";
+import focaccia from "./images/focaccia.jpg";
+import funghi from "./images/funghi.jpg";
+import parma from "./images/parma.jpg";
+import pomodoroEolive from "./images/pomodoro-e-olive.jpg";
+import pomodoroEorigano from "./images/pomodoro-e-origano.jpg";
+
 
 class App extends Component {
+  state = {
+    cards: [
+      {id: 0, nome: "Margherita", prezzo: 4.99, immagine: margherita },
+      {id: 1, nome: "Bianca", prezzo: 3.99, immagine: bianca, quantità: 0 },
+      {id: 2, nome: "Capricciosa", prezzo: 6.99, immagine: capricciosa, quantità: 0 },
+      {id: 3, nome: "Focaccia", prezzo: 2.99, immagine: focaccia, quantità: 0 },
+      {id: 4, nome: "Funghi", prezzo: 5.99, immagine: funghi, quantità: 0 },
+      {id: 5, nome: "Parma", prezzo: 7.99, immagine: parma, quantità: 0 },
+      {id: 6, nome: "Pomodoro e olive", prezzo: 3.99, immagine: pomodoroEolive, quantità: 0 },
+      {id: 7, nome: "Pomodoro e origano", prezzo: 3.99, immagine: pomodoroEorigano, quantità: 0 },
+    ]
+  }
+
   render() {
   return (
     <>
@@ -12,14 +33,11 @@ class App extends Component {
       <h1>Quale pizza preferisci?</h1>
       <hr/>
       <div className="row">
-        <Card nome="Margherita" prezzo={1.99} immagine={margherita} />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+       {this.state.cards.map(card => (
+         <Card key={card.id} nome={card.nome} prezzo={card.prezzo} immagine={card.immagine} />
+       )
+       )
+       }
       </div>
     </div>
     </>
